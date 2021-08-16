d89ed6da82a8beab288dc44c3b887e8ac679b4f3
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-08-16 15:49:37 -0500

    cleaning code

--- coins/src/form.js
+++ coins/src/form.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
 
 class MyForm extends React.Component {
     constructor(props) {
@@ -27,7 +26,7 @@ class MyForm extends React.Component {
         const cents = [1,2,5,10,20,50,100,200];
         const coins = cents.length; 
         const amount = this.state.amount;
-        
+
         return (
             <form>
                 <h1>Amount</h1>
--- coins/src/index.js
+++ coins/src/index.js
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom';
-import Count from './coins';
 import MyForm from './form';
 
 ReactDOM.render(
-  
   <MyForm />
-  
   ,document.getElementById('root')
 );
